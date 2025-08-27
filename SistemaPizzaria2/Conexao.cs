7d94be09c20c627a4c3f935351520b6c6045775b
Wellington <welltecnic@hotmail.com>
2025-08-27 17:27:46 -0300

    Criando a classe de conexao

---
+++ SistemaPizzaria2/Conexao.cs
@@ -0,0 +1,43 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+
+namespace SistemaPizzaria2
+{
+    class Conexao
+    {
+        MySqlConnection con = new MySqlConnection("Data Source=localhost;Initial Catalog=BDPizzaria1;user=root;pwd=12345678");
+
+        public static string msg;
+
+        public MySqlConnection ConnectarBD()
+        {
+            try
+            {
+                con.Open();
+
+            }
+            catch (Exception erro)
+            {
+
+                msg = "Ocorreu um erro ao se conectar" + erro.Message;
+            }
+            return con;
+        }
+
+        public MySqlConnection DesConnectarBD()
+        {
+            try
+            {
+                con.Close();
+
+            }
+            catch (Exception erro)
+            {
+
+                msg = "Ocorreu um erro ao se conectar" + erro.Message;
+            }
+            return con;
+        }
+
+    }
+}
